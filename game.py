d66178af46c2fdd9d7e020e5818960fe3863a53c
Tomislav Skoda <tomislav.skoda@hotelstouch.com>
2020-02-01 17:04:54 +0100

    New guys

--- game.py
+++ game.py
@@ -13,7 +13,7 @@ walkLeft = [pygame.image.load('L.png'), pygame.image.load('L.png'), pygame.image
     'L.png'), pygame.image.load('L.png'), pygame.image.load('L.png'), pygame.image.load('L.png'), pygame.image.load('L.png')]
 
 bg = pygame.image.load('level1.png')
-char = pygame.image.load('guy.png')
+char = [pygame.image.load('guy.png'), pygame.image.load('guy2.png'), pygame.image.load('guy3.png')]
 
 jump = pygame.mixer.Sound('jump.wav')
 maintheme = pygame.mixer.music.load('flow.mp3')
@@ -32,13 +32,17 @@ jumpCount = 10
 left = False
 right = False
 walkCount = 0
+guy_anim = 0
 yspeed = 10
 
 
 def redrawGameWindow():
     global walkCount
+    global guy_anim
 
     win.blit(bg, (0, 0))
+    if(guy_anim) + 1>=3:
+    	guy_anim = 0
     if walkCount + 1 >= 27:
         walkCount = 0
 
@@ -49,7 +53,8 @@ def redrawGameWindow():
         win.blit(walkRight[walkCount//3], (x, y))
         walkCount += 1
     else:
-        win.blit(char, (x, y))
+        win.blit(char[guy_anim], (x, y))
+        guy_anim +=1
         walkCount = 0
 
     pygame.display.update()
